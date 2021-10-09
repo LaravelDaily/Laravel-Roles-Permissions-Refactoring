a020fc50c12c63a64e66f058ced2420ac0ef2842
Nerijus <krekas@gmail.com>
2021-10-09 21:39:08 +0300

    Admin can delete posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('isAdmin')->only('destroy');
+    }
+
     public function index()
     {
         $posts = Post::latest()->paginate();
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,6 +17,14 @@ class UserSeeder extends Seeder
             'password' => Hash::make('password'),
         ]);
 
+        User::create([
+            'name' => 'admin user',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'is_admin' => true,
+        ]);
+
         User::factory(10)->create();
     }
 }
